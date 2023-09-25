80bdaf925aff064ad4267fb698f5c4d19ae01fe5
jokj624 <jokj624@gmail.com>
2023-09-26 00:31:05 +0900

    fix(template): support for exports shortcut

--- src/template.js
+++ src/template.js
@@ -12,28 +12,68 @@ const trim = (target) => {
     return lines.map(line => line.replace(regex, '')).join('\n');
 }
 
+// const requireFunctionTemplate = () =>
+//     `
+//     const moduleCache = {};
+//     const require = moduleName => {
+//         if (moduleCache[moduleName]) {
+//             return moduleCache[moduleName];
+//         }
+//         const exports = {};
+//         moduleCache[moduleName] = exports;
+
+//         modules[moduleName](exports, require);
+//         return moduleCache[moduleName];
+//     };
+// `;
+
 /**
- * require function template
- * 
+ * require function template (exports shortcut)
+ *
  * @returns {string}
  */
-const requireFunctionTemplate = () =>
-    `
-    const moduleCache = {};
+const requireFunctionTemplate = () => 
+`
     const require = moduleName => {
-        if (moduleCache[moduleName]) {
-            return moduleCache[moduleName];
-        }
-        const exports = {};
-        moduleCache[moduleName] = exports;
-
-        modules[moduleName](exports, require);
-        return moduleCache[moduleName];
-    };
+        const { fn, map } = modules[moduleName];
+        const localRequire = path => require(map[path])
+        
+        const module = { exports: {} };
+        fn(localRequire, module, module.exports);
+        return module.exports;
+    }
 `;
 
 /**
- * bundler runtime template
+ * template for transfiled function and dependency map
+ * @param {object} module
+ * @param {string} module.filepath 
+ * @param {string} module.transformedContent
+ * @param {object} module.mapping
+ * 
+ * @returns {string}
+ */
+const moduleTemplate = ({ filePath, transformedContent, mapping }) => 
+`
+    ${JSON.stringify(filePath)}: {
+        fn: (require, module, exports) => {
+            ${transformedContent}
+        },
+        map: ${JSON.stringify(mapping)}
+    }
+`;
+
+/**
+ * template for module map
+ * @param {object[]} modules 
+ * 
+ * @returns {array}
+ */
+const moduleMapTemplate = (modules) => `{${modules.map(module => moduleTemplate(module)).join(',')}}`;
+
+/**
+ * bundler runtime template 
+ * runtimeTemplate(modules: moduleMapTemplate(moduleGraph), entry)
  * @param {object} modules 
  * @param {string} entry 
  * 
@@ -49,4 +89,8 @@ const runtimeTemplate = (modules, entry) => {
 
         kimbapStart({ ${modules}, ${entry} });
     `);
+}
+
+module.exports = {
+    runtimeTemplate,
 }
