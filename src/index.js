07eddc1469f9ad9bf02d8fef62ad96af4e2ff753
gitdog01 <hostdog01@naver.com>
2023-09-29 20:32:16 +0900

    commad Line Interface

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import { dirname, join, resolve } from 'path';
-import { fileURLToPath } from 'url';
-
 import ModuleCompiler from './parser/index.js';
+import CommandLineInterface from './cli/index.js';
 
 class KimbapBundler {
   constructor({ entry, output, ast }) {
@@ -12,20 +10,10 @@ class KimbapBundler {
   // FIXME : 모듈이 정상적으로 컴파일 되는지 테스트 하기 위한 용도
   build() {
     const moduleCompiler = new ModuleCompiler(this.entry);
-    const compiledModule = moduleCompiler.run(ast);
-    console.log(compiledModule);
+    moduleCompiler.run(ast);
   }
 }
 
-// entry 를 밖에서 해줘야함
-// fileReader 가 여기 있는 Options
-const bundlerOptions = {
-    entry: resolve(__dirname, "./origin/index.js"),
-    output: {
-        path: join(__dirname, "./dist"),
-        fileName: "main.js",
-    },
-}
-
+const bundlerOptions = CommandLineInterface();
 const bundler = new KimbapBundler(bundlerOptions);
 bundler.build();
