ec446910dfb22c933a2cd8c1cf952ccd1185af49
Danji-ya <erangerang15@gmail.com>
2023-09-30 12:00:05 +0900

    fix: bundle flow 수정

--- src/index.js
+++ src/index.js
@@ -12,20 +12,37 @@ class KimbapBundler {
   // FIXME : 모듈이 정상적으로 컴파일 되는지 테스트 하기 위한 용도
   build() {
     const moduleCompiler = new ModuleCompiler(this.entry);
-    const compiledModule = moduleCompiler.run(ast);
-    console.log(compiledModule);
+    const { moduleCache, moduleList } = moduleCompiler.run(ast);
+
+    // transform content
+    const transformedModuleList = moduleList.map(
+      ({ ast, fileContent, ...rest }) => ({
+        fileContent: transform(ast, fileContent).transformedContent,
+        ...rest,
+      }),
+    );
+
+    // bundle using template
+    const [entryFile] = moduleCache;
+    const outputContent = runtimeTemplate(
+      moduleMapTemplate(transformedModuleList),
+      entryFile,
+    );
+
+    // create output
+    createOuputFile({ output }, outputContent);
   }
 }
 
 // entry 를 밖에서 해줘야함
 // fileReader 가 여기 있는 Options
 const bundlerOptions = {
-    entry: resolve(__dirname, "./origin/index.js"),
-    output: {
-        path: join(__dirname, "./dist"),
-        fileName: "main.js",
-    },
-}
+  entry: resolve(__dirname, './origin/index.js'),
+  output: {
+    path: join(__dirname, './dist'),
+    fileName: 'main.js',
+  },
+};
 
 const bundler = new KimbapBundler(bundlerOptions);
 bundler.build();
