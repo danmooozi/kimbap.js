096b9d6f8db2827c946e19a2746a6038ae2164ae
Danji-ya <erangerang15@gmail.com>
2023-11-11 14:53:54 +0900

    refactor(transform): 주석 추가

--- src/transform/index.js
+++ src/transform/index.js
@@ -7,6 +7,11 @@ const getActualOptions = (options) =>
 const transform = (ast, content, options) => {
   const { requireAst, requireCode, ...restOptions } = getActualOptions(options);
 
+  /*
+   * babel.transformFromAstSync
+   * 변환 단계에서는 추상 구문 트리(AST)를 받아 그 속을 탐색해 나가며 노드들을 추가, 업데이트, 제거
+   * {@link https://babeljs.io/docs/babel-core#transformfromastsync}
+   */
   const { ast: transformedAst, code: transformedContent } =
     transformFromAstSync(ast, content, {
       ast: requireAst,
--- src/transform/transform-strict-mode.js
+++ src/transform/transform-strict-mode.js
@@ -4,15 +4,23 @@ const visitor = {
   Program: programVisitor,
 };
 
-function programVisitor(path, state) {
+function programVisitor(path, _state) {
   const {
     node: { directives },
   } = path;
 
+  /*
+   * 'use strict' 가 존재하는 경우
+   * 따로 노드를 업데이트하지 않는다.
+   */
   for (const directive of directives) {
     if (directive.value.value === 'use strict') return;
   }
 
+  /*
+   * 'use strict' 가 존재하지 않는 경우
+   * use strict' 노드를 추가한다.
+   */
   path.unshiftContainer(
     'directives',
     t.directive(t.directiveLiteral('use strict')),
